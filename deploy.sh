85c532e04e23de7aa15b9be82518b57d3968b6d1
xtophe02 <moreira.christophe@outlook.com>
2018-11-19 16:09:35 +0100

    kubectl set image

--- deploy.sh
+++ deploy.sh
@@ -14,9 +14,8 @@ docker push xtophe02/robotfriends:$GIT_SHA
 docker push xtophe02/facerecognition:$GIT_SHA
 docker push xtophe02/myportofolio:$GIT_SHA
 
-#kubectl apply -f k8s
-#kubectl set image deployments/robotfriends-service robotfriends=xtophe02/robotfriends:$GIT_SHA
-#kubectl set image deployments/yelpcamp-service yelpcamp=xtophe02/yelpcamp:$GIT_SHA
-#kubectl set image deployments/facerecognition-service facerecognition=xtophe02/facerecognition:$GIT_SHA
-#kubectl set image deployments/facerecognitionapi-service facerecognitionapi=xtophe02/facerecognitionapi:$GIT_SHA
-#kubectl set image deployments/myportofolio-service myportofolio=xtophe02/myportofolio:$GIT_SHA
+kubectl apply -f k8s
+kubectl set image deployments/robotfriends-service robotfriends=xtophe02/robotfriends:$GIT_SHA
+kubectl set image deployments/yelpcamp-service yelpcamp=xtophe02/yelpcamp:$GIT_SHA
+kubectl set image deployments/facerecognition-service facerecognition=xtophe02/facerecognition:$GIT_SHA
+kubectl set image deployments/myportofolio-service myportofolio=xtophe02/myportofolio:$GIT_SHA
